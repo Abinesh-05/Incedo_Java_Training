b4d3d40c2465f3069348e53040ebeb2a14a733e7
Abinesh Vardan <95700050+Abinesh-05@users.noreply.github.com>
2025-01-16 22:46:51 +0530

    Create Sumofdigits.java

---
+++ Sumofdigits.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Sumofdigits{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int sum = 0;
+		while(n > 0)
+		{
+			int t = n%10;
+			sum = sum + t;
+			n = n/10;
+			
+		}
+	    System.out.print(sum);
+	}
+	
+
+}
